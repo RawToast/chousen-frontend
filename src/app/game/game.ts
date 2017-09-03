8e0f4e22c56621aed8c610ac9f16c211da40a12e
rawtoast <jim@jamesmorris.org>
2017-09-03 18:50:42 +0100

    GameComponent reads parameters

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -22,28 +22,19 @@ export class GameComponent implements OnInit {
 
     game: GameResponse = new Defaults().EMPTY_GAME;
 
-
     constructor(private gameService: SharingService,
         private route: ActivatedRoute, private location: Location) { }
 
-    // ngOnInit(): void {
-    //     this.route.paramMap.switchMap((params: ParamMap) =>
-    //       this.gameService.getGame(params.get('id')))
-    //         .subscribe(game => this.game = game);
-    // }
-
     ngOnInit() {
-        // this.route.paramMap.toPromise().then((params: ParamMap) =>
-        //     this.gameService.refresh(params.get('id')));
-
-        this.gameService.awaitData('8a3b0e1c-31c6-4af6-8d2c-13a91c5193c5');
-
-        this.gameService.getData1().subscribe(gs => {
-            if (typeof gs !== 'undefined') {
-                this.game = gs;
-            }
+        this.route.params.subscribe((params: ParamMap) => {
+            const gid = params['id'];
+            console.log('gid' + gid);
+            this.gameService.awaitData(gid);
+            this.gameService.getData1().subscribe(gs => {
+                if (typeof gs !== 'undefined') {
+                    this.game = gs;
+                }
+            });
         });
-
-        console.log('Game ' + this.game);
     }
 }
